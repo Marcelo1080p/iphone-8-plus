91ec3a07601da00ab05f2727c8fe9a514ef1669b
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-24 16:57:31 -0300

    criacao das pastas pages

---
+++ src/Pages/TelaBloqueio/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TELABLOQUEIO.css";
+
+const TELABLOQUEIO = () => {
+    return (
+        <section className="containerBloqueio">
+         
+        </section>
+    )
+};
+
+export default TELABLOQUEIO;
