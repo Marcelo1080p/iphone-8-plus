96c07af34d7332dd42b324cb1370c687d927d707
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-23 23:03:16 -0300

    componente button criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import IPHONE from "./components/IPHONE/index";
+import "./Global.css"
 function App() {
   return (
     <div className="App">
-      
+      <IPHONE/>
     </div>
   );
 }
---
+++ src/components/BOTAO/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BOTAO = (props) => {
+  return <button type={props.type}>{props.nome}</button>;
+};
+
+export default BOTAO;
