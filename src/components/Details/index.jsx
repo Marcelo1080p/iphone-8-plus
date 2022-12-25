f2ada191ffdebd9356e577a25610b2c2aef07274
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-25 19:52:09 -0300

    novas imgs e adicionando elementos ao componente

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -5,7 +5,16 @@ const DETAILS = () => {
     return (
         <section className="containerDetails">
             <span className="sinais">
-                <span></span>
+                <p className="sinalTim">
+                    <div id="div1"></div>
+                    <div id="div2"></div>
+                    <div id="div3"></div>
+                    <div id="div4"></div>
+                </p>
+                <p className="tim">TIM</p>
+                <div className="wifi">
+                        <div className="wifi1"></div>
+                </div>
             </span>
             <span className="bloqueio">
                 <span className="cadeado"></span>
